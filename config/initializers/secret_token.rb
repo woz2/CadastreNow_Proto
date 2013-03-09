7a9ea88315147f1673a9917cec0c8295fcd5f0c0
woz2 <tomaszwoz@yahoo.com>
2013-03-09 17:02:06 +0000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CadastreNow::Application.config.secret_token = '0e0517a9b29e683ab62318c28a1f1050892215f9df88fb66d5d89138df2cbb864d3bef83e0c6a0aecb1741235d5f44de7e82ab2c403b18b80491e9e603227e0c'
